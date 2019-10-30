29d90870dab31134b566bde4a6d35e29b82b4032
Alexander Katkov <katkov.as@students.dvfu.ru>
2019-10-30 11:21:05 +1000

    _

--- cats/FiniteField(Hardversion)/num.cpp
+++ cats/FiniteField(Hardversion)/num.cpp
@@ -1,18 +1,5 @@
 //
-// Created by Alexander on 28.10.2019.
+// Created by Alexander on 29.10.2019.
 //
 
 #include "num.h"
-
-Num::Num(int value, int modulo) {
-    this->value = value;
-    this->modulo = modulo;
-    if (modulo == 0) {
-        this->value = value;
-    } else {
-        this->value = value % modulo;
-    }
-}
-//Num& Num::operator=(const Num &other) {
-//    this->value = other.value;
-//}
--- cats/FiniteField(Hardversion)/num.h
+++ cats/FiniteField(Hardversion)/num.h
@@ -1,34 +1,8 @@
 //
-// Created by Alexander on 28.10.2019.
+// Created by Alexander on 29.10.2019.
 //
 
 #ifndef FINITEFIELD_HARDVERSION__NUM_H
 #define FINITEFIELD_HARDVERSION__NUM_H
-
-#pragma once
-
-class Num {
-public:
-    Num(int value, int modulo);
-
-//    Num& operator=(const Num& other);
-//
-//    Num operator+(const Num& other);
-//    Num operator-(const Num& other);
-//    Num operator*(const Num& other);
-//    Num operator+(int num);
-//    Num operator-(int num);
-//    Num operator*(int num);
-//
-//    Num& operator+=(const Num& other);
-//    Num& operator-=(const Num& other);
-//    Num& operator*=(const Num& other);
-//    Num& operator+=(int num);
-//    Num& operator-=(int num);
-//    Num& operator*=(int num);
-
-    int value;
-    int modulo;
-};
 
 #endif //FINITEFIELD_HARDVERSION__NUM_H
